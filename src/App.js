9345628e52300bf4ae4e7ceeeaddadb382df1bb4
anup <anuppal602@gmail.com>
2022-02-24 00:43:47 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
+import ThemeContextProvider from './context/ThemeContext';
 
 function App() {
   return (
-    <div className="App">
+    <ThemeContextProvider>
       <Navbar />
       <BookList />
-    </div>
+    </ThemeContextProvider>
   );
 }
 
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
 
 class BookList extends Component {
+static contextType = ThemeContext;
   render() { 
+    console.log(this.context);
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+
     return ( 
-      <div className="book-list">
-        <ul>
-          <li>the way of kings</li>
-          <li>the name of the wind</li>
-          <li>the final empire</li>
+      <div className="book-list" style={
+        {
+          background: theme.bg,
+          color: theme.syntax,
+        }
+      }>
+        <ul >
+          <li style={{
+            background: theme.ui,
+          }}>the way of kings</li>
+          <li style={{
+            background: theme.ui,
+          }}>the name of the wind</li>
+          <li style={{
+            background: theme.ui,
+          }}>the final empire</li>
         </ul>
       </div>
     );
